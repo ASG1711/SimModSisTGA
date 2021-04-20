17a820cac4b7d65253481888d4516368363ac5de
Rafael Correa <rafael_c_alves@hotmail.com>
2021-04-19 23:27:25 -0300

    changed presentation test to loop cycle call

--- src/test/java/com/unisinos/petrinet/PFLOWImporterTest.java
+++ src/test/java/com/unisinos/petrinet/PFLOWImporterTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import javax.xml.bind.JAXBException;
 
 public class PFLOWImporterTest {
-    private PFLOWImporter importer = new PFLOWImporter();
+    private final PFLOWImporter importer = new PFLOWImporter();
 
     @Test
     public void importPNMLTest() throws JAXBException {
--- src/test/java/com/unisinos/petrinet/PresentationTest.java
+++ src/test/java/com/unisinos/petrinet/PresentationTest.java
@@ -3,12 +3,13 @@ package com.unisinos.petrinet;
 import com.unisinos.petrinet.models.Document;
 import com.unisinos.petrinet.pflowimport.PFLOWImporter;
 import com.unisinos.petrinet.services.CycleService;
+import org.junit.Assert;
 import org.junit.Test;
 
 import javax.xml.bind.JAXBException;
 
 public class PresentationTest {
-    public static final int REQUISITE_EXAMPLE_FULL_CYCLE = 7;
+    public static final Integer REQUISITE_EXAMPLE_FULL_CYCLE = 7;
 
     private final CycleService cycleService = new CycleService();
     private final PFLOWImporter importer = new PFLOWImporter();
@@ -16,6 +17,7 @@ public class PresentationTest {
     @Test
     public void runRequisitesExample() throws JAXBException {
         Document document = importer.importPNML("exemplo_enunciado.pflow");
-        cycleService.runCycles(document, REQUISITE_EXAMPLE_FULL_CYCLE);
+        Integer totalCycles = cycleService.getCyclesQuantityToFinish(document);
+        Assert.assertEquals(REQUISITE_EXAMPLE_FULL_CYCLE, totalCycles);
     }
 }
